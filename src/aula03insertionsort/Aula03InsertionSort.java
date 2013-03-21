4cd6704b66df78d6920e2561990be448c6eed55a
Lucas <metallucas.ds@gmail.com>
2013-03-21 17:25:44 -0300

    Estrutura do Insertion Sort

--- src/aula03insertionsort/Aula03InsertionSort.java
+++ src/aula03insertionsort/Aula03InsertionSort.java
@@ -4,6 +4,8 @@
  */
 package aula03insertionsort;
 
+import java.util.Scanner;
+
 /**
  *
  * @author AgtLucas
@@ -15,5 +17,28 @@ public class Aula03InsertionSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        int x[] = new int[5];
+        int i, j, eleito;
+ 
+        for (i = 0; i <= 4; i++) {
+            System.out.print("Digite o " + (i + 1) + "° número: ");
+            x[i] = in.nextInt();
+        }
+ 
+        for (i = 1; i <= 4; i++) {
+            eleito = x[i];
+            j = i - 1;
+            while (j >= 0 && x[j] > eleito) {
+                x[j + 1] = x[j];
+                j = j - 1;
+            }
+            x[j + 1] = eleito;
+        }
+ 
+        for (i = 0; i <= 4; i++) {
+            System.out.println((i + 1) + "° número: " + x[i]);
+        }
     }
 }
